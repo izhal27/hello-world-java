e81a29c17c76da8bd7853afda8698e5d7044de8a
Risal Walangadi <risal.walangadi@gmail.com>
2022-01-01 00:32:52 +0800

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        var name = "Risal Walangadi";
+
+        System.out.println("Hello World");
+        System.out.println(String.format("From %1$s", name));
+    }
+}
